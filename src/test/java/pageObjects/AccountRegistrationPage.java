3183ff5ff7813f2c1aad6a347093d5cb49e982b4
NirajShresthaEB <niraj.shrestha@ebpearls.com>
2025-04-16 17:48:00 +0545

    adding locators and methods in home and registration page

--- src/test/java/pageObjects/AccountRegistrationPage.java
+++ src/test/java/pageObjects/AccountRegistrationPage.java
@@ -1,5 +1,83 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AccountRegistrationPage {
+
+	public AccountRegistrationPage(WebDriver driver) {
+		super();
+	}
+
+	// Locators for Webelement of Registration Page
+
+	@FindBy(xpath = "//input[@id='input-firstname']")
+	WebElement txtFirstname;
+
+	@FindBy(xpath = "//input[@id='input-lastname']")
+	WebElement txtLastname;
+
+	@FindBy(xpath = "//input[@id='input-email']")
+	WebElement txtEmail;
+
+	@FindBy(xpath = "//input[@id='input-telephone']")
+	WebElement txtTelephone;
+
+	@FindBy(xpath = "//input[@id='input-password']")
+	WebElement txtPassword;
+
+	@FindBy(xpath = "//input[@id='input-confirm']")
+	WebElement txtConfirmPassword;
+
+	@FindBy(xpath = "//input[@id='input-agree']")
+	WebElement chkdPolicy;
+
+	@FindBy(xpath = "//input[@id='input-continue']")
+	WebElement btnContinue;
+
+	@FindBy(xpath="//h1[normalize-space()="Your Account
+	Has Been Created!")
+	WebElement msgConfirmation;
+
+	public void setFirstName(String fname) {
+		txtFirstname.sendKeys(fname);
+	}
+
+	public void LastName(String lname) {
+		txtLastname.sendKeys(lname);
+	}
+
+	public void setEmail(String email) {
+		txtEmail.sendKeys(email);
+	}
+
+	public void setTelephone(String tel) {
+		txtTelephone.sendKeys(tel);
+	}
+
+	public void setPassword(String pwd) {
+		txtPassword.sendKeys(pwd);
+	}
+
+	public void setConfirmPassword(String pwd) {
+		txtConfirmPassword.sendKeys(pwd);
+	}
+
+	public void setPrivacyPolicy() {
+		chkdPolicy.click();
+	}
+
+	public void clickContinue(String fname) {
+		btnContinue.sendKeys(fname);
+	}
+
+	public String getConfirmationMsg() {
+		try {
+			return (msgConfirmation.getText());
+		} catch (Exception e) {
+			return (e.getMessage());
+		}
+	}
 
 }
--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -1,5 +1,28 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage {
+	
+	public HomePage(WebDriver driver)
+	{
+		super();
+	}
+	
+	@FindBy (xpath="//span[normalize-space='My Account']")
+	WebElement lnkMyAccount;
+	
+	@FindBy (xpath="//a[normalize-space='Register]")
+	WebElement lnkRegister;
+	
+	public void clickMyAccount() {
+		lnkMyAccount.click();
+	}
+	
+	public void clickRegister() {
+		lnkRegister.click();
+	}
 
 }
