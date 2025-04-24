383e756531f8ba6f780359da2a536f65f49d0955
NirajShresthaEB <niraj.shrestha@ebpearls.com>
2025-04-24 17:49:03 +0545

    added cross-browser testing xml and integrated it with Baseclass

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -8,9 +8,11 @@ import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 
@@ -18,11 +20,28 @@ public class BaseClass {
 	public Logger logger;
 
 	@BeforeClass
+	@Parameters({ "os", "browser" })
 
-	public void setup() {
-		
-		logger=LogManager.getLogger(this.getClass());
-		driver = new ChromeDriver();
+	public void setup(String os, String br) {
+
+		logger = (Logger) LogManager.getLogger(this.getClass());
+
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default:
+			System.out.println("Invalid browser nmae ..");
+			return;
+
+		}
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
